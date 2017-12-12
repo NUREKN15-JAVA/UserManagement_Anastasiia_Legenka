f56e52426eac96eb67230742153eb3a72bec17b2
AnastasiiaLegenka <anastasiia.lehenka@nure.ua>
2017-12-12 22:42:32 +0200

    multiagent system added

---
+++ src/main/java/ua/nure/kn156/legenka/agent/SearchException.java
@@ -0,0 +1,21 @@
+package ua.nure.kn156.legenka.agent;
+
+public class SearchException extends Exception {
+
+    public SearchException() {
+        super();
+    }
+
+    public SearchException(String message) {
+        super(message);
+    }
+
+    public SearchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SearchException(Throwable cause) {
+        super(cause);
+    }
+	
+}
